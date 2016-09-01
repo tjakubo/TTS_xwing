eebdf56f4015b95b8e98100f6cad762d8f67843c
dzikakulka <tofix112@gmail.com>
2016-09-02 00:53:49 +0200

    Working prototype of partial movement

--- Global.-1.lua
+++ Global.-1.lua
@@ -28,9 +28,25 @@ end
 --------
 -- VECTOR RELATED FUNCTIONS
 
--- Sum of two 3D vectors
+-- Sum of two vectors (of any size)
 function Vect_Sum(vec1, vec2)
-    return {vec1[1]+vec2[1], vec1[2]+vec2[2], vec1[3]+vec2[3]}
+    local out = {}
+    local k = 1
+    while vec1[k] ~= nil and vec2[k] ~= nil do
+        out[k] = vec1[k]+vec2[k]
+        k = k+1
+    end
+    return out
+end
+
+function Vect_Inverse(vector)
+    local out = {}
+    local k = 1
+    while vector[k] ~= nil do
+        out[k] = -1*vector[k]
+        k = k+1
+    end
+    return out
 end
 
 -- Offset self vector by first and third element of vec2 (second el ignored)
@@ -114,8 +130,12 @@ MoveData.straight[2] = {0, 0, str_mm[2], 0}
 MoveData.straight[3] = {0, 0, str_mm[3], 0}
 MoveData.straight[4] = {0, 0, str_mm[4], 0}
 MoveData.straight[5] = {0, 0, str_mm[5], 0}
-MoveData.straight.smallBaseOffset = {0, 0, mm_smallBase, 0}
-MoveData.straight.largeBaseOffset = {0, 0, mm_largeBase, 0}
+MoveData.straight.smallBaseOffsetInit = {0, 0, mm_smallBase/2, 0}
+MoveData.straight.smallBaseOffsetFinal = {0, 0, mm_smallBase/2, 0}
+MoveData.straight.smallBaseOffset = Vect_Sum(MoveData.straight.smallBaseOffsetInit, MoveData.straight.smallBaseOffsetFinal)
+MoveData.straight.largeBaseOffsetInit = {0, 0, mm_largeBase/2, 0}
+MoveData.straight.largeBaseOffsetFinal = {0, 0, mm_largeBase/2, 0}
+MoveData.straight.largeBaseOffset = Vect_Sum(MoveData.straight.largeBaseOffsetInit, MoveData.straight.largeBaseOffsetFinal)
 
 -- Banks RIGHT (member function to modify to left)
 -- Path traversed is eighth part (1/8 or 45deg) of a circle of specified radius (see CONFIGURATION)
@@ -125,8 +145,12 @@ MoveData.bank = {}
 MoveData.bank[1] = {bankRad_mm[1]-(bankRad_mm[1]/math.sqrt(2)), 0, bankRad_mm[1]/math.sqrt(2), 45}
 MoveData.bank[2] = {bankRad_mm[2]-(bankRad_mm[2]/math.sqrt(2)), 0, bankRad_mm[2]/math.sqrt(2), 45}
 MoveData.bank[3] = {bankRad_mm[3]-(bankRad_mm[3]/math.sqrt(2)), 0, bankRad_mm[3]/math.sqrt(2), 45}
-MoveData.bank.smallBaseOffset = {(mm_smallBase/2)/math.sqrt(2), 0, (mm_smallBase/2)+((mm_smallBase/2)/math.sqrt(2)), 0}
-MoveData.bank.largeBaseOffset = {(mm_largeBase/2)/math.sqrt(2), 0, (mm_largeBase/2)+((mm_largeBase/2)/math.sqrt(2)), 0}
+MoveData.bank.smallBaseOffsetInit = {0, 0, mm_smallBase/2, 0}
+MoveData.bank.smallBaseOffsetFinal = {(mm_smallBase/2)/math.sqrt(2), 0, (mm_smallBase/2)/math.sqrt(2), 0}
+MoveData.bank.smallBaseOffset = Vect_Sum(MoveData.bank.smallBaseOffsetInit, MoveData.bank.smallBaseOffsetFinal)
+MoveData.bank.largeBaseOffsetInit = {0, 0, mm_largeBase/2, 0}
+MoveData.bank.largeBaseOffsetFinal = {(mm_largeBase/2)/math.sqrt(2), 0, (mm_largeBase/2)/math.sqrt(2), 0}
+MoveData.bank.largeBaseOffset = Vect_Sum(MoveData.bank.largeBaseOffsetInit, MoveData.bank.largeBaseOffsetFinal)
 
 -- Turns RIGHT (member function to modify to left)
 -- Path traversed is fourth part (1/4 or 90deg) of a circle of specified radius (see CONFIGURATION)
@@ -136,8 +160,12 @@ MoveData.turn = {}
 MoveData.turn[1] = {turnRad_mm[1], 0, turnRad_mm[1], 90}
 MoveData.turn[2] = {turnRad_mm[2], 0, turnRad_mm[2], 90}
 MoveData.turn[3] = {turnRad_mm[3], 0, turnRad_mm[3], 90}
-MoveData.turn.smallBaseOffset = {mm_smallBase/2, 0, mm_smallBase/2, 0}
-MoveData.turn.largeBaseOffset = {mm_largeBase/2, 0, mm_largeBase/2, 0}
+MoveData.turn.smallBaseOffsetInit = {0, 0, mm_smallBase/2, 0}
+MoveData.turn.smallBaseOffsetFinal = {mm_smallBase/2, 0, 0, 0}
+MoveData.turn.smallBaseOffset = Vect_Sum(MoveData.turn.smallBaseOffsetInit, MoveData.turn.smallBaseOffsetFinal)
+MoveData.turn.largeBaseOffsetInit = {0, 0, mm_largeBase/2, 0}
+MoveData.turn.largeBaseOffsetFinal = {mm_largeBase/2, 0, 0, 0}
+MoveData.turn.largeBaseOffset = Vect_Sum(MoveData.turn.largeBaseOffsetInit, MoveData.turn.largeBaseOffsetFinal)
 
 -- Convert an entry from milimeters to in-game units
 MoveData.ConvertDataToIGU = function(entry)
@@ -165,9 +193,9 @@ end
 -- Apply base offset to an entry
 -- Needs to be done as a member cause both base size and direction affects it
 -- 'info' tells about type and direction, 'is_large' tells if the ship base is large
-MoveData.BaseOffset = function(entry, info, is_large)
+MoveData.BaseOffset = function(entry, info)
     local offset = {}
-    if is_large == true then
+    if info.size == 'large' then
         offset = MoveData[info.type].largeBaseOffset
     else
         offset = MoveData[info.type].smallBaseOffset
@@ -176,49 +204,122 @@ MoveData.BaseOffset = function(entry, info, is_large)
     return {entry[1]+offset[1], entry[2]+offset[2], entry[3]+offset[3], entry[4]+offset[4]}
 end
 
+MoveData.DecodeInfo = function (move_code, ship)
+    local info = {type=nil, speed=nil, dir=nil, extra=nil, size=nil}
+
+    if move_code:sub(1,1) == 's' or move_code:sub(1,1) == 'k' then
+        info.type = 'straight'
+        info.speed = tonumber(move_code:sub(2,2))
+        if move_code:sub(1,1) == 'k' then
+            info.extra = 'koiogran'
+        end
+    elseif move_code:sub(1,1) == 'b' then
+        info.type = 'bank'
+        info.dir = 'right'
+        info.speed = tonumber(move_code:sub(3,3))
+        if move_code:sub(2,2) == 'l' or move_code:sub(2,2) == 'e' then
+            info.dir = 'left'
+        end
+        if move_code:sub(-1,-1) == 's' then
+            info.extra = 'segnor'
+        end
+    elseif move_code:sub(1,1) == 't' then
+        info.type = 'turn'
+        info.dir = 'right'
+        info.speed = tonumber(move_code:sub(3,3))
+        if move_code:sub(2,2) == 'l' or move_code:sub(2,2) == 'e' then
+            info.dir = 'left'
+        end
+        if move_code:sub(-1,-1) == 't' then
+            info.extra = 'talon'
+        elseif move_code:sub(-1,-1) == 's' then
+            info.extra = 'segnor'
+        end
+    end
+    -- check database
+    -- else
+    if ship.getName():find('LGS') ~= nil then info.size = 'large'
+    else info.size = 'small' end
+    return info
+end
+
 -- Decode a "move" from the standard X-Wing notation into a valid movement data
 -- Provide a 'ship' object reference to determine if it is large based
 --- TO_DO: actually check if base is large (include database and failsafe LGS in name handling)
 --- TO_DO: barrel rolls, decloaks and all this shit
 -- Returns a valid path the ship has to traverse to perform a move (if it was at origin)
 -- Standard format {xOffset, yOffset, zOffset, rotOffset}
-MoveData.Decode = function(move_code, ship)
+MoveData.DecodeFull = function(move_code, ship)
     local data = {}
-    local info = {}
+    local info = MoveData.DecodeInfo(move_code, ship)
 
-    if move_code:sub(1,1) == 's' or move_code:sub(1,1) == 'k' then
-        info = {type='straight', dir=nil}
-        data = Lua_ShallowCopy(MoveData.straight[tonumber(move_code:sub(2,2))])
-        if move_code:sub(1,1) == 'k' then
-            data = MoveData.TurnAroundVariant(data)
-        end
-    elseif move_code:sub(1,1) == 'b' then
-        info = {type='bank', dir='right'}
-        data = Lua_ShallowCopy(MoveData.bank[tonumber(move_code:sub(3,3))])
-        if move_code:sub(2,2) == 'l' or move_code:sub(2,2) == 'e' then
-            data = MoveData.LeftVariant(data)
-            info.dir = 'left'
-        end
-        if move_code:sub(-1,-1) == 's' then
-            data = MoveData.TurnAroundVariant(data)
-        end
-    elseif move_code:sub(1,1) == 't' then
-        info = {type='turn', dir='right'}
-        data = Lua_ShallowCopy(MoveData.turn[tonumber(move_code:sub(3,3))])
-        if move_code:sub(2,2) == 'l' or move_code:sub(2,2) == 'e' then
-            data = MoveData.LeftVariant(data)
-            info.dir = 'left'
-        end
-        if move_code:sub(-1,-1) == 't' then
-            data = MoveData.TurnInwardVariant(data)
-        end
+    data = Lua_ShallowCopy(MoveData[info.type][info.speed])
+    if info.dir == 'left' then data = MoveData.LeftVariant(data) end
+    if info.extra == 'koiogran' or info.extra == 'segnor' then
+        data = MoveData.TurnAroundVariant(data)
+    elseif info.extra == 'talon' then
+        data = MoveData.TurnInwardVariant(data)
     end
-    if ship == nil then
-        data = MoveData.BaseOffset(data, info, false)
-    end
+    data = MoveData.BaseOffset(data, info)
+
     return MoveData.ConvertDataToIGU(data)
 end
 
+MoveData.DecodePartial = function(move_code, ship, part)
+    local data = {}
+    local info = MoveData.DecodeInfo(move_code, ship)
+
+    if part >= 1000 then return MoveData.DecodeFull(move_code, ship)
+    elseif part < 0 then return {0, 0, 0, 0} end
+
+    if info.type == 'straight' then
+        data = MoveData.DecodeFull(move_code, ship)
+        if part < 1000 then data[4] = 0 end
+        data[3] = data[3]*(part/1000)
+    else
+        if part <= 100 then
+            local offset = MoveData.ConvertDataToIGU(MoveData[info.type][info.size .. 'BaseOffsetInit'])[3]*(part/100)
+            local angle = MoveData[info.type][info.speed][4]*(part/1000)
+            if info.dir == 'left' then angle = -1*angle end
+            data = {0, 0, offset, angle}
+        elseif part > 900 then
+            data = MoveData.DecodeFull(move_code, ship)
+            data[4] = MoveData[info.type][info.speed][4]*(part/1000)
+            if info.dir == 'left' then data[4] = -1*data[4] end
+
+            part = (part-1000)*(-1/100)
+
+            local xInset = MoveData.ConvertDataToIGU(MoveData[info.type][info.size .. 'BaseOffsetFinal'])[1]*part
+            local zInset = MoveData.ConvertDataToIGU(MoveData[info.type][info.size .. 'BaseOffsetFinal'])[3]*part
+            if info.dir == 'left' then xInset = xInset * -1 end
+            data[1] = data[1] - xInset
+            data[3] = data[3] - zInset
+        else
+            local angle = MoveData[info.type][info.speed][4]*(part/1000)
+            if info.dir == 'left' then angle = -1*angle end
+            data = Lua_ShallowCopy(MoveData[info.type][info.size .. 'BaseOffsetInit'])
+            part = (part-100)*(10/8)
+            local fullArg = nil
+            if info.type == 'bank' then fullArg = math.pi/4
+            elseif info.type == 'turn' then fullArg = math.pi/2 end
+            local arg = (fullArg*part)/1000
+            local radius = {}
+            if info.type == 'bank' then radius = bankRad_mm[info.speed]
+            elseif info.type == 'turn' then radius = turnRad_mm[info.speed] end
+            local yoff = math.sin(arg)*radius
+            local xoff = (-1*math.cos(arg)+1)*radius
+            if info.dir == 'left' then xoff = xoff * -1 end
+
+            data[1] = data[1] + xoff
+            data[3] = data[3] + yoff
+            data[4] = angle
+            data = MoveData.ConvertDataToIGU(data)
+        end
+
+    end
+
+    return data
+end
 
 -- Table containing all the high-level functions used to move stuff around
 MoveModule = {}
@@ -226,25 +327,52 @@ MoveModule = {}
 -- Simply get the final position for a 'ship' if it did a 'move' (standard move code)
 -- Format:  out.pos for position, out.rot for rotation
 -- Position and rotation are ready to feed TTS functions
-MoveModule.getFinalPos = function(ship, move)
-    local finalPos = MoveData.Decode(move)
+MoveModule.GetFinalPos = function(move, ship)
+    local finalPos = MoveData.DecodeFull(move, ship)
     local finalRot = finalPos[4] + ship.getRotation()[2]
     finalPos = Vect_RotateDeg(finalPos, ship.getRotation()[2]+180)
     finalPos = Vect_Offset(ship.getPosition(), finalPos)
     return {pos=finalPos, rot={0, finalRot, 0}}
 end
 
+MoveModule.GetPartialPos = function(move, ship, part)
+    local finalPos = MoveData.DecodePartial(move, ship, part)
+    local finalRot = finalPos[4] + ship.getRotation()[2]
+    finalPos = Vect_RotateDeg(finalPos, ship.getRotation()[2]+180)
+    finalPos = Vect_Offset(ship.getPosition(), finalPos)
+    return {pos=finalPos, rot={0, finalRot, 0}}
+end
+
+MoveModule.restWaitQuota = {}
+
+
 -- END MAIN MOVEMENT MODULE
 --------
 
+iter = 0
+spos = {}
+srot = {}
+sobj = nil
 -- Sample update to test functionalities
 function update()
-    for k,obj in pairs(getAllObjects()) do
-        if obj.tag == 'Figurine' and obj.getDescription() ~= '' then
-            local finalPos = MoveModule.getFinalPos(obj, obj.getDescription())
-            obj.setPosition(finalPos.pos)
-            obj.setRotation(finalPos.rot)
-            obj.setDescription('')
+    if sobj == nil then
+        for k,obj in pairs(getAllObjects()) do
+            if obj.tag == 'Figurine' and obj.getDescription() ~= '' then
+                --[[local finalPos = MoveModule.GetFinalPos(obj.getDescription(), obj)
+                obj.setPosition(finalPos.pos)
+                obj.setRotation(finalPos.rot)
+                obj.setDescription('')]]--
+                sobj = obj
+            end
         end
+    else
+        if iter == 0 then spos = sobj.getPosition() srot = sobj.getRotation() end
+        sobj.setPosition(spos)
+        sobj.setRotation(srot)
+        local finalPos = MoveModule.GetPartialPos(sobj.getDescription(), sobj, iter)
+        sobj.setPosition(finalPos.pos)
+        sobj.setRotation(finalPos.rot)
+        iter = iter+10
+        if iter > 1000 then iter = 0 sobj.setDescription('') sobj = nil end
     end
 end
